1d0a004516cf3bc3ba0de7bd33a4923a0ec3c30a
ad9570 <loufs_@naver.com>
2022-08-10 13:08:01 +0700

    \\ \"

---
+++ CodingTest/src/step1/Main13.java
@@ -0,0 +1,15 @@
+package step1;
+
+public class Main13 {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("\\    /\\\n");
+		sb.append(" )  ( ')\n");
+		sb.append("(  /  )\n");
+		sb.append(" \\(__)|");
+		
+		System.out.println(sb);
+	}
+
+}
---
+++ CodingTest/src/step1/Main15.java
@@ -0,0 +1,17 @@
+package step1;
+
+public class Main15 {
+
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("         ,r'\"7\n");
+		sb.append("r`-_   ,'  ,/\n");
+		sb.append(" \\. \". L_r'\n");
+		sb.append("   `~\\/\n");
+		sb.append("      |\n");
+		sb.append("      |");
+		
+		System.out.println(sb);
+	}
+	
+}
