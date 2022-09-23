1c247c4af0331a481ab4eeb04f98d2ccd6451509
ad9570 <loufs_@naver.com>
2022-09-23 18:55:12 +0700

    day21

---
+++ CodingTest/src/step8/Main1.java
@@ -0,0 +1,36 @@
+package step8;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main1 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int test = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int prime = 0;
+		
+		primeTest:
+		for (int i = 0; i < test; i++) {
+			int number = Integer.parseInt(st.nextToken());
+			
+			if (number == 1 || (number % 2 == 0 && number != 2))
+				continue;
+			
+			if (number % 6 == 1 || number % 6 == 5) {
+				for (int j = 3; j <= Math.sqrt(number); j += 2) {
+					if (number % j == 0)
+						continue primeTest;
+				}
+				
+				prime++;
+			}
+		}
+		
+		System.out.println(prime);
+	}
+
+}
