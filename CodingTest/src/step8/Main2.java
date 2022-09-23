e28cb75cb7ff79634b3fe78fd4ab118bf36dde5b
ad9570 <loufs_@naver.com>
2022-09-23 22:10:29 +0700

    day 21

--- CodingTest/src/step8/Main2.java
+++ CodingTest/src/step8/Main2.java
@@ -3,7 +3,6 @@ package step8;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class Main2 {
 
@@ -11,13 +10,14 @@ public class Main2 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int min = Integer.parseInt(br.readLine());
 		int max = Integer.parseInt(br.readLine());
-		int sum = 0;
-		ArrayList<Integer> prime = new ArrayList<>();
+		int sum = 0, first = 0;
 		
 		if (min < 3) {
 			min = 3;
-			if (max >= 2)
-				prime.add(2);
+			if (max >= 2) {
+				sum = 2;
+                first = 2;
+            }
 		}
 		
 		primeTest:
@@ -27,17 +27,15 @@ public class Main2 {
 					continue primeTest;
 			}
 			
-			prime.add(i);
+			sum += i;
+            first = first == 0 ? i : first;
 		}
 		
-		if (prime.size() == 0) {
+		if (sum == 0) {
 			System.out.println(-1);
 		} else {
-			for (int i : prime)
-				sum += i;
-			
 			System.out.println(sum);
-			System.out.println(prime.get(0));
+			System.out.println(first);
 		}
 	}
 
